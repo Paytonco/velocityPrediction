19c2399e61b09ae4db7d74b9bb0eb594e686e212
Payton Thomas <paytonco@gmail.com>
2023-05-29 15:07:13 -0600

    script to download data

---
+++ downloadDat.py
@@ -0,0 +1,8 @@
+import scvelo as scv
+
+scv.datasets.pancreas()
+scv.datasets.dentategyrus()
+scv.datasets.forebrain()
+#scv.datasets.gastrulation()
+scv.datasets.bonemarrow()
+scv.datasets.pbmc68k()
