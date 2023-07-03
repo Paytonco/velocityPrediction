8e31611c8edcc62a2dca938c8547d3562cb051a8
Taos Transue <spacechips@rocketmail.com>
2023-07-03 19:59:03 +0000

    no zero noise

--- bifurcationExample.py
+++ bifurcationExample.py
@@ -4,10 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def genPoint(epsilon):
-    t = 10*random.uniform(0,1)
+    t = 6*random.uniform(0,1)
     x0 = 0
-    y0 = 1 + epsilon * random.uniform(-1,1)
-
+    y0 = 1 + epsilon * random.uniform(.5,1) * np.sign(random.uniform(-1, 1))
     x = t + x0
     y = (y0-1) * math.exp(0.1*t**2+x0*t) + 1
     v = (1,0.2*x*(y-1))
@@ -23,6 +22,7 @@ for i in range(numPts):
 
 (T,X,Y) = (ptsArray[:,0],ptsArray[:,1],ptsArray[:,2])
 (Vx,Vy) = (vecArray[:,0],vecArray[:,1])
+breakpoint()
 plt.scatter(X,Y,c=T)
 
 plt.quiver(X,Y,Vx,Vy,T)
