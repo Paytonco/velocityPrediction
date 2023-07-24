f5119a3f72420e139d88148b3c8ce30e3708ad6d
Payton Thomas <paytonco@gmail.com>
2023-07-24 16:59:29 -0600

    changes to downloadDat.py

--- downloadDat.py
+++ downloadDat.py
@@ -1,8 +1,20 @@
 import scvelo as scv
 
+"""
 scv.datasets.pancreas()
 scv.datasets.dentategyrus()
 scv.datasets.forebrain()
 #scv.datasets.gastrulation()
 scv.datasets.bonemarrow()
 scv.datasets.pbmc68k()
+"""
+
+adata = scv.datasets.pancreas()
+scv.pp.filter_and_normalize(adata)
+scv.pp.moments(adata)
+scv.tl.velocity(adata, mode='stochastic')
+scv.pl.velocity_graph(adata)
+scv.pl.velocity_embedding(adata, basis='umap')
+scv.tl.velocity_pseudotime(adata)
+mat = adata.X
+breakpoint()
