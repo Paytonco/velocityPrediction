78da307f9eef3680c774fbc7f7fd67eb337595eb
Taos Transue <spacechips@rocketmail.com>
2023-07-25 14:32:17 -0700

    add some printouts of adata, and a requirements.txt file for scVelo compatibility

--- downloadDat.py
+++ downloadDat.py
@@ -13,7 +13,10 @@ adata = scv.datasets.pancreas()
 scv.pp.filter_and_normalize(adata)
 scv.pp.moments(adata)
 scv.tl.velocity(adata, mode='stochastic')
-scv.pl.velocity_graph(adata)
-scv.pl.velocity_embedding(adata, basis='umap')
+
+# must call velocity_pseudotime before velocity_graph
+scv.tl.velocity_graph(adata)
 scv.tl.velocity_pseudotime(adata)
+
+scv.tl.velocity_embedding(adata, basis='umap')
 breakpoint()
