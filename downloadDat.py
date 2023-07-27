debfc15f957bc6902de602c452cf06f7deeba370
Taos Transue <spacechips@rocketmail.com>
2023-07-27 14:50:36 -0700

    add pancreas data

--- downloadDat.py
+++ downloadDat.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 import scvelo as scv
+import numpy as np
+import pandas as pd
 
 """
 scv.datasets.pancreas()
@@ -9,14 +12,39 @@ scv.datasets.bonemarrow()
 scv.datasets.pbmc68k()
 """
 
-adata = scv.datasets.pancreas()
-scv.pp.filter_and_normalize(adata)
-scv.pp.moments(adata)
-scv.tl.velocity(adata, mode='stochastic')
+DATASETS = [
+    'pancreas'
+    'dentategyrus',
+    'forebrain',
+    'gastrulation',
+    'bonemarrow',
+    'pbmc68k',
+]
+
+
+def get_csvs(name, basis_dim=2):
+    folder = Path(f'data/{name}')
+    adata = getattr(scv.datasets, name)(folder / f'{name}.h5ad')
+    scv.pp.filter_and_normalize(adata)
+    scv.pp.moments(adata)
+    scv.tl.velocity(adata, mode='stochastic')
 
 # must call velocity_pseudotime before velocity_graph
-scv.tl.velocity_graph(adata)
-scv.tl.velocity_pseudotime(adata)
+    scv.tl.velocity_graph(adata)
+    scv.tl.velocity_pseudotime(adata)
 
-scv.tl.velocity_embedding(adata, basis='umap')
-breakpoint()
+    scv.tl.velocity_embedding(adata, basis='umap')
+
+    pseudotime = adata.obs.velocity_pseudotime
+    positions = adata.obsm['X_umap']
+    velocities = adata.obsm['velocity_umap']
+    data = pd.DataFrame(
+        data=np.concatenate((positions, velocities), axis=1),
+        index=pseudotime.rename('t'),
+        columns=['x1', 'x2', 'v1', 'v2']
+    )
+    data.to_csv(folder / f'{name}.csv')
+
+
+for ds in DATASETS:
+    get_csvs(ds)
