adding sparsification, and correcting vector scaling in plots

datasets.py
@@ -44,9 +44,6 @@ class SlidingWindowGraph(T.BaseTransform):
         self.window_size = window_size
 
     def __call__(self, data):
-        sort = data.t.argsort()
-        for a in ('t', 'pos', 'vel'):
-            data[a] = data[a][sort]
         assert (data.t.diff() >= 0).all(), 'Time must be in non-decreasing order.'
         windows = torch.arange(data.t.numel()).unfold(0, self.window_size, 1)
         mask_poi = torch.zeros(self.window_size, dtype=bool)
@@ -64,9 +61,10 @@ class NeighborsDataset(InMemoryDataset):
     seed = 0
     generator = torch.Generator()
 
-    def __init__(self, set_neighbors_transform, path=None):
+    def __init__(self, set_neighbors_transform, sparsity_step, path=None):
         super().__init__(None)
         self.set_neighbors_transform = set_neighbors_transform
+        self.sparsity_step = sparsity_step
         self.path = path
         self.generator.manual_seed(self.seed)
         data = self.load_data()
@@ -77,7 +75,10 @@ class NeighborsDataset(InMemoryDataset):
         raise NotImplementedError
 
     def points_to_data(self, t, pos, vel):
-        t, pos, vel = t[::2], pos[::2], vel[::2]
+        sort = t.argsort()
+        t, pos, vel = t[sort], pos[sort], vel[sort]
+        step = self.sparsity_step
+        t, pos, vel = t[::step], pos[::step], vel[::step]
         data = Data(t=t, pos=pos, vel=vel)
         return data
 

main.py
@@ -9,6 +9,7 @@ import torch_geometric as tg
 import torch_geometric.nn as tg_nn
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
+from matplotlib.colors import LinearSegmentedColormap
 
 import datasets
 
@@ -59,18 +60,16 @@ def plot_true_vs_inferred(out, data, neighbor_lines=True):
     if neighbor_lines:
         poi_pos = data.poi_pos[data.batch]
         diff_pos = F.normalize(data.pos - poi_pos, dim=1)
-        ax.quiver(poi_pos[:, 0], poi_pos[:, 1],
-                  diff_pos[:, 0], diff_pos[:, 1],
+        ax.quiver(poi_pos[:, 0], poi_pos[:, 1], diff_pos[:, 0], diff_pos[:, 1],
                   color='red', headwidth=1)
         # lc = LineCollection(torch.stack((poi_pos, poi_pos + diff_pos), dim=1), linestyles='-', colors='r')
         # ax.add_collection(lc)
-    ax.scatter(data.poi_pos[:, 0], data.poi_pos[:, 1],
-               label='State', c='orange')  # data.poi_t)
-    ax.quiver(data.poi_pos[:, 0], data.poi_pos[:, 1],
-              data.poi_vel[:, 0], data.poi_vel[:, 1], label='True', color='deepskyblue')
-    ax.quiver(data.poi_pos[:, 0], data.poi_pos[:, 1],
-              out[:, 0], out[:, 1], label='Inferred', color='blue')
-    ax.legend()
+    ax.scatter(data.poi_pos[:, 0], data.poi_pos[:, 1], label='State', c='orange')
+    cm = LinearSegmentedColormap.from_list('true_inferred', ['deepskyblue', 'blue'])
+    pos = data.poi_pos.repeat(2, 1)
+    vel = torch.cat((data.poi_vel, out))
+    color_vals = torch.tensor([0., 1.]).repeat_interleave(data.poi_pos.size(0))
+    ax.quiver(pos[:, 0], pos[:, 1], vel[:, 0], vel[:, 1], color=cm(color_vals))
     ax.set_xlabel('$x$')
     ax.set_ylabel('$y$')
     return fig