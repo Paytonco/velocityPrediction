4c5e51b99e4a47259d0a193787343a725ca4ad32
Taos Transue <spacechips@rocketmail.com>
2024-04-15 20:17:29 -0600

    adding model architecture parameters

--- src/models.py
+++ src/models.py
@@ -7,19 +7,26 @@ import utils
 
 
 class First(nn.Module):
-    def __init__(self):
+    def __init__(self, cfg):
         super().__init__()
-        self.weighter = nn.Sequential(
-            nn.Linear(2, 10),
-            nn.ReLU(),
-            nn.Linear(10, 20),
-            nn.ReLU(),
-            nn.Linear(20, 10),
-            nn.ReLU(),
-            nn.Linear(10, 5),
-            nn.ReLU(),
-            nn.Linear(5, 1),
-        )
+        self.weighter = nn.Sequential()
+        dims = [2, *([cfg.hidden.dim] * cfg.hidden.layers), 1]
+        for layer, (d_in, d_out) in enumerate(zip(dims, dims[1:])):
+            self.weighter.append(nn.Linear(d_in, d_out, bias=cfg.bias))
+            if layer < len(dims) - 2:
+                self.weighter.append(getattr(nn, cfg.activation)())
+
+        # self.weighter = nn.Sequential(
+        #     nn.Linear(2, 10),
+        #     nn.ReLU(),
+        #     nn.Linear(10, 20),
+        #     nn.ReLU(),
+        #     nn.Linear(20, 10),
+        #     nn.ReLU(),
+        #     nn.Linear(10, 5),
+        #     nn.ReLU(),
+        #     nn.Linear(5, 1),
+        # )
 
     def forward(self, t, pos, poi_t, poi_pos, batch):
         diff_t = torch.sign(t - poi_t[batch])
@@ -35,6 +42,6 @@ def get_model(cfg, rng_seed=0):
     with pl.utilities.seed.isolate_rng():
         pl.seed_everything(rng_seed, workers=True)
         if cfg.name == 'First':
-            return First()
+            return First(cfg)
         else:
             raise ValueError(f'Unknown model: {cfg.name}')
