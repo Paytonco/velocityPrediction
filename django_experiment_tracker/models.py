fa2116ff513ee93fb9fd3262c399a6ec90e9e799
Taos Transue <spacechips@rocketmail.com>
2026-05-19 08:51:27 -0600

    db improvements

--- django_experiment_tracker/models.py
+++ django_experiment_tracker/models.py
@@ -32,12 +32,19 @@ class GitCommit(models.Model):
     branch = models.CharField(max_length=50)
     commit_sha = models.CharField(max_length=40)
 
+    def __str__(self):
+        return f'{self.commit_time}: {self.branch} ({self.commit_sha})'
+
 
 class ParameterEnum(models.Model):
     """
     Enum values assignable to a parameter.
     """
-    parameter_enum_description = models.CharField(max_length=100)
+    parameter_enum_name = models.CharField(max_length=100, unique=True)
+    parameter_enum_description = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.parameter_enum_name
 
 
 class ParameterEnumValue(models.Model):
@@ -45,14 +52,32 @@ class ParameterEnumValue(models.Model):
     One value in the enumeration of a parameter enum.
     """
     parameter_enum = models.ForeignKey(ParameterEnum, on_delete=models.CASCADE)
+    parameter_enum_value = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.parameter_enum_value
 
 
 class Parameter(models.Model):
     """
     A parameter assingable to an experiment.
     """
-    parameter_name = models.CharField(max_length=100)
-    parameter_enum = models.ForeignKey(ParameterEnum, on_delete=models.PROTECT)
+    parameter_name = models.CharField(max_length=100, unique=True)
+    parameter_description = models.CharField(max_length=100, blank=True)
+    parameter_enum = models.ForeignKey(ParameterEnum, blank=True, null=True, on_delete=models.PROTECT)
+    parameter_default_value = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.parameter_name} ({self.parameter_default_value})'
+
+
+class ParameterGroup(models.Model):
+    parameter_group_name = models.CharField(max_length=100, unique=True)
+    parameter_group_description = models.CharField(max_length=100, blank=True)
+    parameters = models.ManyToManyField(Parameter, blank=True)
+
+    def __str__(self):
+        return self.parameter_group_name
 
 
 class Experiment(models.Model):
@@ -60,28 +85,12 @@ class Experiment(models.Model):
 
     alt_id = models.CharField(max_length=8+len(PREFIX_ALT_ID), unique=True, editable=False, db_default=db_default_random_string(4, PREFIX_ALT_ID))
     git_commit = models.ForeignKey(GitCommit, on_delete=models.CASCADE, related_name='%(app_label)s_%(class)s_related', related_query_name='%(app_label)s_%(class)ss')
-    time_created = models.DateTimeField(db_default=models.functions.Now())
+    time_created = models.DateTimeField(blank=True, db_default=models.functions.Now())
     time_completed = models.DateTimeField(blank=True, null=True)
     exit_code = models.IntegerField(blank=True, null=True)
 
     class Meta:
         abstract = True
-
-    # def save(self, *args, **kwargs):
-    #     if not self.alt_id:
-    #         self.alt_id = f"{self.PREFIX_ALT_ID}_{generate_unique(type(self), 'alt_id')}"
-    #     return super().save(*args, **kwargs)
-
-
-# class ExperimentParameter(models.Model):
-#     experiment = models.ForeignKey(Experiment, on_delete=models.PROTECT)
-#     parameter = models.ForeignKey(Parameter, on_delete=models.PROTECT)
-#     pk = models.CompositePrimaryKey('experiment', 'parameter')
-#     parameter_value = models.CharField(max_length=100)
-#
-#     class Meta:
-#         abstract = True
-
 
 # class OptunaOptimizationDirection(models.TextChoices):
 #     MINIMIZE = ('min', _('Minimize'))
--- rna_vel_pred/models.py
+++ rna_vel_pred/models.py
@@ -1,13 +1,76 @@
+from django.contrib import admin
 from django.db import models
 from django_experiment_tracker import models as tracker_models
+
+
+@admin.register(tracker_models.GitCommit)
+class GitCommitAdmin(admin.ModelAdmin):
+    pass
+
+
+# @admin.register(tracker_models.ParameterEnumValue)
+# class ParameterEnumValueAdmin(admin.ModelAdmin):
+#     pass
+
+class ParameterEnumValueInline(admin.TabularInline):
+    model = tracker_models.ParameterEnumValue
+    min_num = 1
+    extra = 0
+
+
+@admin.register(tracker_models.ParameterEnum)
+class ParameterEnumAdmin(admin.ModelAdmin):
+    inlines = [
+        ParameterEnumValueInline,
+    ]
+
+
+class ParameterGroupInline(admin.TabularInline):
+    model = tracker_models.ParameterGroup.parameters.through
+    min_num = 1
+    extra = 0
+
+
+@admin.register(tracker_models.Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    inlines = [
+        ParameterGroupInline,
+    ]
+
+
+@admin.register(tracker_models.ParameterGroup)
+class ParameterGroupAdmin(admin.ModelAdmin):
+    pass
 
 
 class Experiment(tracker_models.Experiment):
     pass
 
+    def __str__(self):
+        parameter_group_names = tracker_models.ParameterGroup.objects.filter(experimentparameter__in=self.experimentparameter_set.all()).values_list("parameter_group_name", flat=True).distinct()
+        return f'{self.alt_id} ({", ".join(parameter_group_names)})'
+
 
 class ExperimentParameter(models.Model):
     experiment = models.ForeignKey(Experiment, on_delete=models.PROTECT)
     parameter = models.ForeignKey(tracker_models.Parameter, on_delete=models.PROTECT)
-    pk = models.CompositePrimaryKey('experiment', 'parameter')
+    parameter_group = models.ForeignKey(tracker_models.ParameterGroup, on_delete=models.PROTECT)
     parameter_value = models.CharField(max_length=100)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['experiment', 'parameter', 'parameter_group'], name='experiment_parameter_and_group_alt_key'),
+        ]
+
+
+class ExperimentParameterAdmin(admin.TabularInline):
+    model = ExperimentParameter
+    min_num = 1
+    extra = 0
+
+
+@admin.register(Experiment)
+class ExperimentAdmin(admin.ModelAdmin):
+    inlines = [
+        ExperimentParameterAdmin,
+    ]
