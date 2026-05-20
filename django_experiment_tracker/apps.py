41ee7612de83c8d6cfe29a9a985c46e79a515d33
Taos Transue <spacechips@rocketmail.com>
2026-05-20 11:36:17 -0600

    make experiment tracker a reusable app

--- django_experiment_tracker/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoExperimentTrackerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_experiment_tracker'
