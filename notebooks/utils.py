9332ea098e00fdc85becf5b885f49fa396fdcca5
Taos Transue <spacechips@rocketmail.com>
2026-08-13 10:18:00 -0600

    update utils

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,9 +3,12 @@ import sys
 import pathlib
 
 
-def initialize_django_in_notebook(settings_module='project.settings', PROJECT_DIR=pathlib.Path(__file__).resolve().parent.parent):
+DIR_ROOT = pathlib.Path(__file__).resolve().parent.parent
+
+
+def initialize_django_in_notebook(settings_module='project.settings', project_dir=DIR_ROOT):
     # Add the project root to the sys.path so you can import your Django app modules
-    sys.path.append(str(PROJECT_DIR))
+    sys.path.append(str(project_dir))
 
     # Set your settings module and allow async operations
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
