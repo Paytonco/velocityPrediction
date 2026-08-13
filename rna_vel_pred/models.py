7cec410495aad58b91134c9a01e134b53c1c54ce
Taos Transue <spacechips@rocketmail.com>
2026-08-13 10:19:44 -0600

    add savedfile model

--- rna_vel_pred/models.py
+++ rna_vel_pred/models.py
@@ -2,24 +2,27 @@ from django.db import models
 from django_experiment_tracker import models as tracker_models
 
 
-class Experiment(tracker_models.Experiment):
-    pass
+class SavedDatasetFile(tracker_models.SharedFile):
+    def __str__(self):
+        parameter_group_names = tracker_models.ParameterGroup.objects.filter(saveddatasetfileparameter__in=self.saveddatasetfileparameter_set.all()).values_list("parameter_group_name", flat=True).distinct()
+        return f'{self.alt_id} ({", ".join(parameter_group_names)})'
 
+
+class SavedDatasetFileParameter(tracker_models.ParameterValue):
+    saved_dataset_file = models.ForeignKey(SavedDatasetFile, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = tracker_models.ParameterValue.get_constraints('saved_dataset_file')
+
+
+class Experiment(tracker_models.Experiment):
     def __str__(self):
         parameter_group_names = tracker_models.ParameterGroup.objects.filter(experimentparameter__in=self.experimentparameter_set.all()).values_list("parameter_group_name", flat=True).distinct()
         return f'{self.alt_id} ({", ".join(parameter_group_names)})'
 
 
-class ExperimentParameter(models.Model):
+class ExperimentParameter(tracker_models.ParameterValue):
     experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
-    parameter_group = models.ForeignKey(tracker_models.ParameterGroup, on_delete=models.CASCADE)
-    parameter = models.ForeignKey(tracker_models.Parameter, on_delete=models.CASCADE)
-    parameter_value = models.CharField(max_length=100)
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['experiment', 'parameter', 'parameter_group'], name='experiment_parameter_and_group_alt_key'),
-        ]
-
-    def __str__(self):
-        return f'{self.parameter_group}, {self.parameter}, {self.parameter_value}'
+        constraints = tracker_models.ParameterValue.get_constraints('experiment')
